84aa4a82d485c6977d3dde9eea3ede15b604123f
Роман Козырев <Tyuiu.KozyrevRA@outlook.com>
2025-09-30 19:06:43 +0500

    Add project files.

---
+++ Tyuiu.KozyrevRA.Sprint3.Task0.V12.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.KozyrevRA.Sprint3.Task0.V12.Lib
+{
+    public class DataService : ISprint3Task0V12
+    {
+        public double GetMultiplySeries(int value, int startValue, int stopValue)
+        {
+            double MultSeries = 1.0;
+            int i;
+            for(i = startValue; i <= stopValue; i++)
+            {
+                MultSeries *= (Math.Pow(value, i) + (1.0 / (1.0 + i)));
+            }
+
+            return Math.Round(MultSeries, 3);
+        }
+    }
+}
---
+++ Tyuiu.KozyrevRA.Sprint3.Task0.V12/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KozyrevRA.Sprint3.Task0.V12.Lib;
+namespace Tyuiu.KozyrevRA.Sprint3.Task0.V12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Козырев Р. А. | СМАРТБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема:  Оператор цикла for                                               *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #12                                                             *");
+            Console.WriteLine("* Выполнил: Козырев Роман Артёмович | СМАРТБ-25-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл for, которая вычисляет                *");
+            Console.WriteLine("* произведение ряда по формуле, при t = 1                                 *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            int value = 1;
+            int startValue = 1;
+            int stopValue = 13;
+            Console.WriteLine("Переменная X = " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Произведение ряда = " + ds.GetMultiplySeries(value, startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
