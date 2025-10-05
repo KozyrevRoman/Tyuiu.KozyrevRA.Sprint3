b656af2195b97ab155435750b7fa5cdccf4dc8b3
Роман Козырев <Tyuiu.KozyrevRA@outlook.com>
2025-10-05 20:23:28 +0500

    Task5

---
+++ Tyuiu.KozyrevRA.Sprint3.Task5.V28.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.KozyrevRA.Sprint3.Task5.V28.Lib;
+namespace Tyuiu.KozyrevRA.Sprint3.Task5.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSumSeries()
+        {
+            DataService ds = new DataService();
+            double res = ds.GetSumSumSeries(2, 1, 1, 3, 12);
+            double wait = -1032.182;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
